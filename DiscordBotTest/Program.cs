33d1b597d1743584f2dfb05a3c1c3aa13236f1e4
Grenka <64643423+Grhead@users.noreply.github.com>
2022-08-02 22:43:05 +0700

    Reg command modules

--- DiscordBotTest/Program.cs
+++ DiscordBotTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using DSharpPlus;
+﻿using DiscordBotTest.Commands;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
 
 DiscordConfiguration DSConf = new DiscordConfiguration()
 {
@@ -10,6 +12,14 @@ DiscordConfiguration DSConf = new DiscordConfiguration()
 static async Task MainAsync(DiscordConfiguration DSConf)
 {
     var discord = new DiscordClient(DSConf);
+    var commands = discord.UseCommandsNext(
+    new CommandsNextConfiguration()
+    {
+        StringPrefixes = new[] { "!" },
+        CaseSensitive = false
+    });
+    commands.RegisterCommands<TrainingModule>();
+    commands.RegisterCommands<StaffModule>();
     await discord.ConnectAsync();
     await Task.Delay(-1);
 }
