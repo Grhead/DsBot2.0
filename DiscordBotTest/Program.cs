722371d655925d1cbf48a13b3673cba4592bf84a
Grenka <64643423+Grhead@users.noreply.github.com>
2022-08-02 18:27:39 +0700

    Rep created. Base command folder

---
+++ DiscordBotTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+
+DiscordConfiguration DSConf = new DiscordConfiguration()
+{
+    //Token = Environment.GetEnvironmentVariable("LORDBOTTOKEN"),
+    Token = "Nzc5OTE2NTc2OTc5NDE5MTM2.X7nf6g.24Wlk_i2XXuC1VDVBodKE9WHms8",
+    TokenType = TokenType.Bot
+};
+
+
+static async Task MainAsync(DiscordConfiguration DSConf)
+{
+    var discord = new DiscordClient(DSConf);
+    await discord.ConnectAsync();
+    await Task.Delay(-1);
+
+
+}
+
+
+MainAsync(DSConf).GetAwaiter().GetResult();
